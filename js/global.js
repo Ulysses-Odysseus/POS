248ca46a70a7b8733ccc1204e9b0b5553e0f44e2
Ulysses Jimenez <u88jimenez@gmail.com>
2015-10-01 16:39:35 -0700

    Finish slider, and responsive settings for it in js, working now in responsivness of page

--- js/global.js
+++ js/global.js
@@ -1,28 +1,31 @@
 $(document).ready(function(){
    $('.testimonials-slider').slick({
-		arrows: true,
+		arrows: false,
 		centerMode: true,
-		centerPadding: '60px',
+		centerPadding: '10px',
 		slidesToShow: 3,
-		// responsive: [
-		// {
-		// 	breakpoint: 768,
-		// 	settings: {
-		// 	  arrows: false,
-		// 	  centerMode: true,
-		// 	  centerPadding: '40px',
-		// 	  slidesToShow: 3
-		// 	}
-		// },
-		// {
-		// 	breakpoint: 480,
-		// 	settings: {
-		// 	  arrows: false,
-		// 	  centerMode: true,
-		// 	  centerPadding: '40px',
-		// 	  slidesToShow: 1
-		// 	}
-		// }
-		// ]
+		autoplay: true,
+		autoplaySpeed: 5000,
+
+		responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+			  arrows: false,
+			  centerMode: true,
+			  centerPadding: '10px',
+			  slidesToShow: 3
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+			  arrows: false,
+			  centerMode: true,
+			  centerPadding: '1px',
+			  slidesToShow: 1
+			}
+		}
+		]
    });
 });
